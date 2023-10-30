7ca7b26abbbc64f2c62086bdba3ad7b76d3fbeec
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-10-30 19:59:34 +0700

    Update authLayout.tsx

--- src/_auth/authLayout.tsx
+++ src/_auth/authLayout.tsx
@@ -14,7 +14,7 @@ export default function AuthLayout() {
           </section>
 
           <img
-            src="/assets/images/side-img.svg"
+            src="/assets/images/side-img.jpg"
             alt="logo"
             className="hidden md:block h-screen w-1/2 object-cover bg-no-repeat"
           />
