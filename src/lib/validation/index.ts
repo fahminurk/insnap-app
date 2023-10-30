fb31e21815250533b809588413c9eef50d494a09
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-10-30 19:59:49 +0700

    update: lib validation zod

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -8,3 +8,9 @@ export const SignUpSchema = z.object({
     .string()
     .min(8, { message: "Password must be at least 8 characters" }),
 });
+export const SignInSchema = z.object({
+  email: z.string().email({ message: "Invalid email" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" }),
+});
