715d901da3c4c391960e606f5d918fb76e73f8b0
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-11-01 16:48:09 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import AuthLayout from "./_auth/authLayout";
 import { SigninForm, SignupForm } from "./_auth/forms";
-import { Home } from "./_root/pages";
+import {
+  AllUsers,
+  CreatePost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdatePost,
+  UpdateProfile,
+} from "./_root/pages";
 import RootLayout from "./_root/rootLayout";
 import { Toaster } from "./components/ui/toaster";
 import "./globals.css";
@@ -19,6 +29,14 @@ function App() {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<UpdatePost />} />
+          <Route path="/post/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
