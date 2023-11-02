df399c7bad8444088da69c665edb910bb1c54572
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-11-02 10:33:47 +0700

    update: ts

--- src/lib/react-query/postQueries.ts
+++ src/lib/react-query/postQueries.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import {
   useQuery,
   useMutation,
@@ -137,10 +138,11 @@ export const useGetPosts = () => {
   return useInfiniteQuery({
     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
     queryFn: getInfinitePosts,
+    // @ts-ignore
     getNextPageParam: (lastPage) => {
       if (lastPage && lastPage.documents.length === 0) return null;
 
-      const lastId = lastPage.documents[lastPage?.documents.length - 1].$id;
+      const lastId = lastPage?.documents[lastPage.documents.length - 1].$id;
       return lastId;
     },
   });
